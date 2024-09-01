ca44232856fa80f0486b48cdb10b5241e27718ec
Paulo <paulomarcelonds@gmail.com>
2024-09-01 18:07:42 -0300

    convertendo tipos

---
+++ convertendo_tipos.py
@@ -0,0 +1,12 @@
+print(int(10.5))
+print(int('150'))
+print(float('100.20'))
+print(float(1000))
+
+valor = 500
+valor_string = str(valor)
+print(type(valor))
+print(type(valor_string))
+
+print(1000 / 2)
+print(1000 // 2)
