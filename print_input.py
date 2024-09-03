c258d2f3a63d387703ebb4de9b55c38e40924d52
paulomarcelonds <paulomarcelonds@gmail.com>
2024-09-03 13:01:52 -0300

    arquivo print_input

---
+++ print_input.py
@@ -0,0 +1,7 @@
+nome = input('Qual o seu nome?:  ')
+idade = input('Qual a sua idade?:  ')
+
+print(nome, idade)
+print(nome, idade, end= '...\n')
+print(nome, idade, sep= '#', end= '...\n') 
+print(nome, idade, end='#')
